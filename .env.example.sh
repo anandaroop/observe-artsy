a7948b6f78c74f01fda73dea891a2974a60922cb
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-03-18 14:44:08 -0400

    chore: simplify env

--- .env.example.sh
+++ .env.example.sh
@@ -1,8 +1,12 @@
+### REDSHIFT
+
 export PGHOST=REPLACE
 export PGPORT=REPLACE
 export PGDATABASE=REPLACE
 export PGUSER=REPLACE
+export PGPASSWORD=REPLACE
 
-# locally, will use 1password to inject the password into the env
-ONE_PASSWORD_REDSHIFT_ITEM="REPLACE"
-export PGPASSWORD=$(op item get "${ONE_PASSWORD_REDSHIFT_ITEM}" --fields "${PGUSER}")
+### SUPABASE (for color quiz)
+
+export SUPABASE_KEY=REPLACE
+export SUPABASE_URL=REPLACE
